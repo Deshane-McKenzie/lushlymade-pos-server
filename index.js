eaf38ff969f6952156695525f83732b7f73cff17
Deshane McKenzie <122234024+Deshane-McKenzie@users.noreply.github.com>
2023-09-21 07:13:44 -0400

    deshanes changes

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express= require('express');
-const app=express();
 const cors= require('cors')
-require('dotenv').config()
-const { CORS_ORIGIN, PORT, BACKEND_URL } = process.env;
+const app=express();
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8000;
+
+//Allows client sidde to connect to server side without issues
+app.use(cors());
+
+//Used to access public folder for images
+app.use(express.static('public'));
+
 const productRoute=require("./routes/productRoute")
 app.use("/", productRoute)
 app.listen(PORT, ()=>{
